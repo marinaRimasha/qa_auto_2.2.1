05d59605cca2bfcc14ecb30a03792cac820beb46
marinaRimasha <rimasa.marina@gmail.com>
2024-04-24 21:10:56 +0300

    homework/2.2.2

--- calculations.js
+++ calculations.js
@@ -1,30 +1,21 @@
 function increaseBalance(x, y) {
-  x = x + y;
-  return x;
+  return (x += y);
 }
 
 function decreaseBalance(x, y) {
-  //had to add y as it was not defined here
-  x = x - y;
-  return x;
+  return (x -= y);
 }
 
 function divideBalanceByAccounts(x, y) {
-  x = x / y;
   console.log("x");
-  /**this was unreachable as stated after return ir you wanted to log x 
-  if you wanted to log x value and not "x" string, you should not use ""
-  */
-  return x;
+  return (x /= y);
 }
 
 function getRestAfterDivision(x, y) {
-  x = x % y;
-  return x;
-} //this one looks fine to me
+  return (x %= y);
+}
 
 console.log(increaseBalance(3000, 700));
 console.log(decreaseBalance(3000, 700));
 console.log(divideBalanceByAccounts(3000, 2));
-// third argument is not stated in the method
 console.log(getRestAfterDivision(7000, 3));
